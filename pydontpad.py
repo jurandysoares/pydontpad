928667c25ce4bffaba754c187c7e8e48fd39bab0
Jurandy Soares <jurandy.soares@gmail.com>
2018-11-04 22:21:13 -0300

    Working version.

--- pydontpad.py
+++ pydontpad.py
@@ -1,12 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
+import os.path
+import tempfile
+import time
 
 BASE_URL = 'http://dontpad.com'
 
 class DontPad:
     def __init__(self, path):
-        self.path = path
-        self.url = '/'.join([BASE_URL, path])
+        self.path = path if path.endswith('/') else path+'/'
+        self.url = '/'.join([BASE_URL, self.path])
         self._session = requests.Session()
 
     def _update(self):
@@ -19,29 +22,43 @@ class DontPad:
         self._update()
         return (self._text)
 
-    def get_zip(self, file):
-        pass
+    def get_zip(self, folder=''):
+        if not folder or not os.path.exists(folder):
+            folder = tempfile.gettempdir()
+        zip_path = self.path[:-1]+'.zip'
+        zip_url = self.url[:-1]+'.zip'
+        zip_fname = os.path.basename(zip_path)
+        response = self._session.get(zip_url)
+        destiny = os.path.join(folder, zip_fname)
+        with open(destiny, 'bw') as zip_file:
+            zip_file.write(response.content)
+        return destiny
 
-    def set_text(self, text):
+    def set_text(self, text, append=False):
         self._update()
         post_data = {
-            'text': text, 
+            'text': self._text+text if append else text, 
             'lastUpdate': self._last_update
             }
         requests.post(self.url, data=post_data)        
 
     def append_text(self, text):
-        self._update()
-        self.set_text(self._text+text)
+        self.set_text(self._text+text, append=True)
+
+    def get_subfolders(self):
+        return []
 
 
-def main():
+def test():
     jurandy = DontPad('jurandy')
     print('The current text is:\n' + jurandy.get_text())
-    jurandy.set_text('I works!')
-    jurandy.append_text('\nAnd it should work for ever and ever.')
+    time.sleep(1)
+    jurandy.set_text('It works!')
+    time.sleep(1)
+    jurandy.append_text('\nAnd it should work while dontpad uses the same form fields.')
+    time.sleep(1)
     print('The updated text now is:\n' + jurandy.get_text())
 
 if __name__ == '__main__':
-    main()
+    test()
 
